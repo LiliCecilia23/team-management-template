5fb3700831789098bfe8161a14c1a7412c61891e
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-09-28 11:11:39 -0500

    finished manager contructor

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+class Manager extends Employee {
+    constructor(name, email, id, officeNum){
+        super(name, email, id);
+        this.officeNum = officeNum;
+    };
+    getName(){
+        return this.name;
+    };
+    getId(){
+        return this.id;
+    };
+    getEmail(){
+        return this.email;
+    };
+    getofficeNum(){
+        return this.officeNum;
+    };
+    getRole(){
+        return 'Manager';
+    };
+};
+
+module.exports = Manager;
