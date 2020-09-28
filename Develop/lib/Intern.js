b999806d05f56550d16d8c9d36eeaf261195c1bb
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-09-28 11:09:28 -0500

    finished intern constructor

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+class Intern extends Employee {
+    constructor(name, email, id, school){
+        super(name, email, id);
+        this.school = school;
+    };
+    getName(){
+        return this.name;
+    };
+    getId(){
+        return this.id;
+    };
+    getEmail(){
+        return this.email;
+    };
+    getSchool(){
+        return this.school;
+    };
+    getRole(){
+        return 'Intern';
+    };
+};
+
+module.exports = Intern;
